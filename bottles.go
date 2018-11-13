b6975115ae1d8d07a2857428fc98ae9e1d12b83e
Jonathan Lewis <jon@lewis.engineer>
2018-11-12 19:45:26 -0700

    solve zero bottles of beer

--- bottles.go
+++ bottles.go
@@ -35,5 +35,12 @@ func (b Bottle) verse(x int) string {
 		startOfLastLine,
 		nextBottleDescription)
 
+	if x == 0 {
+		song = "No more bottles of beer on the wall, " +
+			"no more bottles of beer.\n" +
+			"Go to the store and buy some more, " +
+			"99 bottles of beer on the wall.\n"
+	}
+
 	return song
 }
