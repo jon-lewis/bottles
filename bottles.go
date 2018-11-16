1a1e4270c6e987c492758f99160a9652b993501c
Jonathan Lewis <jon@lewis.engineer>
2018-11-15 19:57:10 -0700

    refactor default and verse 2

--- bottles.go
+++ bottles.go
@@ -32,16 +32,20 @@ func (b Bottle) verse(number int) string {
 			"1 bottle of beer.\n" +
 			"Take it down and pass it around, " +
 			"no more bottles of beer on the wall.\n"
-	case 2:
-		return "2 bottles of beer on the wall, " +
-			"2 bottles of beer.\n" +
-			"Take one down and pass it around, " +
-			"1 bottle of beer on the wall.\n"
 	default:
 		return fmt.Sprintf("%d bottles of beer on the wall, "+
 			"%d bottles of beer.\n"+
 			"Take one down and pass it around, "+
-			"%d bottles of beer on the wall.\n",
-			number, number, number-1)
+			"%d %s of beer on the wall.\n",
+			number, number, number-1, container(number-1))
+	}
+}
+
+func container(number int) string {
+	switch number {
+	case 1:
+		return "bottle"
+	default:
+		return "bottles"
 	}
 }
