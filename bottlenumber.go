3855dc70565fc5a8c85d4894b3c103774e6b3460
Jonathan Lewis <jon@lewis.engineer>
2018-11-30 20:10:20 -0700

    refactor names

--- bottlenumber.go
+++ bottlenumber.go
@@ -7,35 +7,35 @@ type BottleNumber struct {
 	number int
 }
 
-func (bn BottleNumber) successor() int {
-	switch bn.number {
+func (bottleNumber BottleNumber) successor() int {
+	switch bottleNumber.number {
 	case 0:
 		return 99
 	default:
-		return bn.number - 1
+		return bottleNumber.number - 1
 	}
 }
 
-func (bn BottleNumber) action() string {
-	switch bn.number {
+func (bottleNumber BottleNumber) action() string {
+	switch bottleNumber.number {
 	case 0:
 		return "Go to the store and buy some more, "
 	default:
-		return fmt.Sprintf("Take %s down and pass it around, ", bn.pronoun())
+		return fmt.Sprintf("Take %s down and pass it around, ", bottleNumber.pronoun())
 	}
 }
 
-func (bn BottleNumber) quantity() string {
-	switch bn.number {
+func (bottleNumber BottleNumber) quantity() string {
+	switch bottleNumber.number {
 	case 0:
 		return "no more"
 	default:
-		return fmt.Sprintf("%d", bn.number)
+		return fmt.Sprintf("%d", bottleNumber.number)
 	}
 }
 
-func (bn BottleNumber) pronoun() string {
-	switch bn.number {
+func (bottleNumber BottleNumber) pronoun() string {
+	switch bottleNumber.number {
 	case 1:
 		return "it"
 	default:
@@ -43,8 +43,8 @@ func (bn BottleNumber) pronoun() string {
 	}
 }
 
-func (bn BottleNumber) container() string {
-	switch bn.number {
+func (bottleNumber BottleNumber) container() string {
+	switch bottleNumber.number {
 	case 1:
 		return "bottle"
 	default:
--- bottles.go
+++ bottles.go
@@ -9,31 +9,31 @@ import (
 // Bottle is used to construct the verses in the 99 bottles song.
 type Bottle struct{}
 
-func (b Bottle) song() string {
-	return b.verses(99, 0)
+func (bottle Bottle) song() string {
+	return bottle.verses(99, 0)
 }
 
-func (b Bottle) verses(x int, y int) string {
-	song := b.verse(x)
+func (bottle Bottle) verses(x int, y int) string {
+	song := bottle.verse(x)
 
 	for i := x - 1; i >= y; i-- {
-		song += "\n" + b.verse(i)
+		song += "\n" + bottle.verse(i)
 	}
 
 	return song
 }
 
 // Verse method sings the 99 bottles song
-func (b Bottle) verse(number int) string {
-	bn := BottleNumber{number}
-	sbn := BottleNumber{bn.successor()}
+func (bottle Bottle) verse(number int) string {
+	bottleNumber := BottleNumber{number}
+	nextBottleNumber := BottleNumber{bottleNumber.successor()}
 
 	return fmt.Sprintf("%s %s of beer on the wall, "+
 		"%s %s of beer.\n"+
 		"%s"+
 		"%s %s of beer on the wall.\n",
-		words.Capitalize(bn.quantity()), bn.container(),
-		bn.quantity(), bn.container(),
-		bn.action(),
-		sbn.quantity(), sbn.container())
+		words.Capitalize(bottleNumber.quantity()), bottleNumber.container(),
+		bottleNumber.quantity(), bottleNumber.container(),
+		bottleNumber.action(),
+		nextBottleNumber.quantity(), nextBottleNumber.container())
 }
